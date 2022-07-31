02d4cddd42ab9d1f54ef7eb683c3b837ec69afa7
xolar1989 <xolar1989@o2.pl>
2022-07-31 17:02:29 +0200

    add script for installing aws dependencies

---
+++ setup-ec2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+
+# download aws cli version 2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+source "$HOME/.bashrc"
+
+echo  "WARNING/ Need restart machine"
